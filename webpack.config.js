fab5e93183489589383aba39ea845f696b161cd8
John Abrahams <jabrah23@gmail.com>
2020-10-30 11:06:30 -0400

    First iteration at configuring Webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path")
+
+module.exports = {
+  entry: "./css/base/branding.css",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          { loader: "css-loader", options: { importLoaders: 1 } },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                ident: 'postcss',
+                plugins: [
+                  require('tailwindcss'),
+                  require('autoprefixer'),
+                ],
+              },
+            }
+          },
+        ],
+      },
+    ],
+  }
+}
