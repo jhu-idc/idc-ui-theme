a80d8998d47ac28f533d13d4f485efe35269ef72
Jared Galanis <jaredgalanis@gmail.com>
2020-11-05 12:30:16 -0500

    move all js into js and theme files to root

---
+++ js/packages/stimulus/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    stimulus: "./index.js"
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+  ]
+}
