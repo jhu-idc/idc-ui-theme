9a8ea598b49526fe0f0a9b26328112a7cecd6e38
John Abrahams <jabrah23@gmail.com>
2021-02-09 10:38:27 -0500

    Stub placeholder functions to get labels

--- js/packages/glimmer-idc/src/utils/facet-utils.ts
+++ js/packages/glimmer-idc/src/utils/facet-utils.ts
@@ -75,3 +75,54 @@ export function facetValueIncludes(facet: FacetValue, array: FacetValue[]): bool
 export function removeSelectedItem(item: FacetValue, selectedFacets: FacetValue[]): FacetValue[] {
   return selectedFacets.filter((value) => !facetValueEquals(value, item));
 }
+
+/**
+ * Get labels for facet fields from Drupal's JSONAPI. These field keys are used as
+ * json property keys in the facet response
+ *
+ * Example of a facet field: field_access_rights
+ *
+ * TODO: terrible hack because Drupal is pain to deal with
+ *
+ * @param keys list of field keys that need labels
+ * @returns Map of field keys -> label
+ */
+function getFieldLabels(keys: string[]): Map<string, string> {
+  let result = new Map<string, string>();
+  const regex = /_/g;
+
+  keys.forEach((str: string) => result.set(str, str.replace(regex, ' ')));
+
+  return result;
+}
+
+/**
+ * Notes for getting labels
+ *
+ * Facet values can be dereferenced to human readable labels. This configuration can is set
+ * for each facet. You can either let the facets return entity IDs, or the "system" can
+ * resolve labels for entity IDs. Returning the entity IDs makes it easy to generate search
+ * queries, whereas returning labels makes it much easier to render to a user.
+ *
+ * If you get entity IDs, how do you get labels from Drupal?
+ * ??
+ * It _may_ be possible to query various taxonomies and stuff using the Drupal JSONAPI, however,
+ * there may not be an easy way to determine the JSON API request...
+ *
+ * If you get the labels, how do you get the proper search query?
+ * Some horrible URL parsing to pull off the appended facet query parameter from the facet
+ * value URL :(
+ *
+ * There is the extra of how to present the facet fields to the user. Each facet category
+ * comes from a Drupal field on a data model. So far I haven't found a way to externally
+ * query Drupal for these labels. May need to do some garbage in PHP :(
+ */
+
+/**
+ *
+ * @param url url target returned with the facet data
+ * @returns {string}
+ */
+function getFacetQueryFragment(url: string): string {
+  return '';
+}
