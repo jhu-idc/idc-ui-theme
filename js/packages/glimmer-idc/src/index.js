51e8ae004d2494eb0031a0a759ff820227396b9c
John Abrahams <jabrah23@gmail.com>
2021-03-30 15:41:35 -0400

    Add back collection drawer and btn group

--- js/packages/glimmer-idc/src/index.js
+++ js/packages/glimmer-idc/src/index.js
@@ -1,8 +1,12 @@
 import { renderComponent } from '@glimmerx/core';
 import { ITEM_TYPES, ResultsService } from './utils/results';
 import IDCSearch, { ELEMENT_ID } from './components/idc-search';
+import AboutCollectionDrawer from './components/about-collection-drawer';
+import AboutCollectionButtonGroup from './components/about-collection-button-group';
 
-const searchTarget = document.getElementById(ELEMENT_ID)
+const searchTarget = document.getElementById(ELEMENT_ID);
+const aboutCollectionDrawerTarget = document.getElementById('about-collection-drawer');
+const aboutCollectionButtonGroupTarget = document.getElementById('about-collection-button-group');
 
 if (searchTarget) {
   const type = searchTarget.dataset.type;
@@ -15,4 +19,9 @@ if (searchTarget) {
       }
     });
   }
+}
+
+if (aboutCollectionDrawerTarget && aboutCollectionButtonGroupTarget) {
+  renderComponent(AboutCollectionDrawer, aboutCollectionDrawerTarget);
+  renderComponent(AboutCollectionButtonGroup, aboutCollectionButtonGroupTarget);
 }
