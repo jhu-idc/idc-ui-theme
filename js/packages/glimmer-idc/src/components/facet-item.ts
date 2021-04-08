cfe127119245a002809750a18bb60688519b2155
John Abrahams <jabrah23@gmail.com>
2021-04-08 13:46:11 -0400

    Left align facets

--- js/packages/glimmer-idc/src/components/facet-item.ts
+++ js/packages/glimmer-idc/src/components/facet-item.ts
@@ -29,7 +29,7 @@ export default class FacetItem extends Component<Args> {
   }
 
   static template = hbs`
-    <button {{on "click" this.selectFacet}} class="py-1 px-3 flex items-center rounded-full text-black hover:bg-gray-200 {{if this.isSelected "bg-blue-spirit"}}">
+    <button {{on "click" this.selectFacet}} class="py-1 px-3 flex items-center rounded-full text-left text-black hover:bg-gray-200 {{if this.isSelected "bg-blue-spirit"}}">
       {{@item.value}}&nbsp;
       <span class="mr-3">({{@item.count}})</span>
       {{#if this.isSelected}}
