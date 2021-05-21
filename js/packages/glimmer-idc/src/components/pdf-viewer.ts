071a1b4a36948212ff154e58b944c07c3048f59e
Jared Galanis <jaredgalanis@gmail.com>
2021-05-21 06:58:03 -0700

    display pdf viewer without audio media

--- js/packages/glimmer-idc/src/components/pdf-viewer.ts
+++ js/packages/glimmer-idc/src/components/pdf-viewer.ts
@@ -1,12 +1,10 @@
 import Component, { hbs, tracked } from '@glimmerx/component';
-import { action, on } from '@glimmerx/modifier';
-import { is } from '../utils/helpers';
 
 interface Args {}
 
 export default class PdfViewer extends Component<Args> {
   @tracked itemUuid: string = '';
-  @tracked isOralHistory: boolean = false;
+  @tracked isPdf: boolean = false;
   @tracked pdfUrl: string = '';
 
   constructor(owner: unknown, args: Args) {
@@ -19,21 +17,18 @@ export default class PdfViewer extends Component<Args> {
   }
 
   async setupViewer() {
-    let res = await fetch(`/jsonapi/media/audio?filter[media-filter][condition][value]=${this.itemUuid}&filter[media-filter][condition][operator]=IN&filter[media-filter][condition][path]=field_media_of.id`);
-    const audioPayload = await res.json();
-
-    res = await fetch(`/jsonapi/media/document?include=field_media_document&filter[media-filter][condition][value]=${this.itemUuid}&filter[media-filter][condition][operator]=IN&filter[media-filter][condition][path]=field_media_of.id&filter[pdf-filter][condition][value]=application/pdf&filter[pdf-filter][condition][operator]=IN&filter[pdf-filter][condition][path]=field_mime_type`);
+   let res = await fetch(`/jsonapi/media/document?include=field_media_document&filter[media-filter][condition][value]=${this.itemUuid}&filter[media-filter][condition][operator]=IN&filter[media-filter][condition][path]=field_media_of.id&filter[pdf-filter][condition][value]=application/pdf&filter[pdf-filter][condition][operator]=IN&filter[pdf-filter][condition][path]=field_mime_type`);
     const pdfPayload = await res.json();
 
-    if (audioPayload.data.length > 0 && pdfPayload.data.length > 0) {
-      this.isOralHistory = true;
+    if (pdfPayload.data.length > 0) {
+      this.isPdf = true;
 
-      this.pdfUrl = pdfPayload.included[0].attributes.uri.url;
+      this.pdfUrl = `/themes/contrib/idc-ui-theme/js/packages/pdf.js/web/viewer.html?file=${pdfPayload.included[0].attributes.uri.url}`;
     }
   }
 
   static template = hbs`
-    {{#if this.isOralHistory}}
+    {{#if this.isPdf}}
       <iframe class="w-full my-4 h-pdfjs-container" src={{this.pdfUrl}}>
       </iframe>
     {{/if}}
