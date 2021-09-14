5df98d248bcd2958aa7d25d6588fbced97e44c72
Jared Galanis <jaredgalanis@gmail.com>
2021-09-14 09:45:54 -0400

    Add export collection button; handle missing citations

--- js/packages/glimmer-idc/src/components/citations.ts
+++ js/packages/glimmer-idc/src/components/citations.ts
@@ -2,7 +2,7 @@ import Component, { hbs, tracked } from '@glimmerx/component';
 import { CitationsResponse } from '../interfaces';
 
 export default class Citations extends Component {
-  @tracked citations: CitationsResponse;
+  @tracked citations: CitationsResponse | void[];
   @tracked nodeId: string = '';
 
   constructor(owner: unknown, args) {
@@ -24,22 +24,28 @@ export default class Citations extends Component {
 
   static template = hbs`
     <div class="flex flex-col space-y-8 p-8">
-      {{#each this.citations as |citation|}}
-        <div class="flex flex-col space-y-4">
-          <div class="flex flex-col space-y-1">
-            <div class="text-gray-600">APA Citation</div>
-            <div>{{{citation.citation_apa}}}</div>
+      {{#if this.citations}}
+        {{#each this.citations as |citation|}}
+          <div class="flex flex-col space-y-4">
+            <div class="flex flex-col space-y-1">
+              <div class="text-gray-600">APA Citation</div>
+              <div>{{{citation.citation_apa}}}</div>
+            </div>
+            <div class="flex flex-col space-y-1">
+              <div class="text-gray-600">Chicago Citation</div>
+              <div>{{{citation.citation_chicago}}}</div>
+            </div>
+            <div class="flex flex-col space-y-1">
+              <div class="text-gray-600">MLA Citation</div>
+              <div>{{{citation.citation_mla}}}</div>
+            </div>
           </div>
-          <div class="flex flex-col space-y-1">
-            <div class="text-gray-600">Chicago Citation</div>
-            <div>{{{citation.citation_chicago}}}</div>
-          </div>
-          <div class="flex flex-col space-y-1">
-            <div class="text-gray-600">MLA Citation</div>
-            <div>{{{citation.citation_mla}}}</div>
-          </div>
+        {{/each}}
+      {{else}}
+        <div class="text-gray-600">
+          There are no citations for this item.
         </div>
-      {{/each}}
+      {{/if}}
     </div>
   `;
 }
