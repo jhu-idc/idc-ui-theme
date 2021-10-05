b9d2f1490f884e75fad40e74df4d68579178fe2c
Jared Galanis <jaredgalanis@users.noreply.github.com>
2021-10-05 16:07:13 -0400

    Prod builds (#68)
    - update build commands to pass prod flag to yarn build cmd
    - add yarn build-dev cmd
    - update tailwind to latest release
    - update tailwind config to purge unused css

--- js/packages/stimulus/webpack.config.js
+++ js/packages/stimulus/webpack.config.js
@@ -1,25 +1,29 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: {
-    stimulus: './index.js',
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+module.exports = () => {
+  const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+
+  return {
+    mode: IS_PRODUCTION ? 'production' : 'development',
+    entry: {
+      stimulus: './index.js',
+    },
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
         },
-      },
-    ],
-  },
-  plugins: [new CleanWebpackPlugin({ cleanStaleWebpackAssets: false })],
+      ],
+    },
+    plugins: [new CleanWebpackPlugin({ cleanStaleWebpackAssets: false })],
+  }
 };
