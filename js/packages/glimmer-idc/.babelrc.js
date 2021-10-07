f8d099d782ebc60229091c232f9471598bfd2fb3
Jared Galanis <jaredgalanis@gmail.com>
2021-10-07 14:43:40 -0400

    fix babel cache issue

--- js/packages/glimmer-idc/.babelrc.js
+++ js/packages/glimmer-idc/.babelrc.js
@@ -1,11 +1,7 @@
 module.exports = function (api) {
+  api.cache(true);
+
   return {
-    plugins: [
-      ['@glimmer/babel-plugin-glimmer-env', { DEBUG: !api.env('production') }],
-      '@glimmerx/babel-plugin-component-templates',
-      ['@babel/plugin-proposal-decorators', { legacy: true }],
-      '@babel/plugin-proposal-class-properties',
-    ],
-    presets: ['@babel/preset-env', '@babel/preset-typescript'],
+    presets: ['@glimmer/babel-preset', '@babel/preset-env', '@babel/preset-typescript'],
   };
 };
