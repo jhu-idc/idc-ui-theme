a70f48c84d042c85de7137d0f581660de91a921e
Don Richards <highlander411@gmail.com>
2022-12-05 16:37:35 -0500

    Dockerize

--- js/autobuild.sh
+++ js/autobuild.sh
@@ -1,15 +1,22 @@
-#! /bin/sh
+#!/usr/bin/env bash
 
 pwd
 
 yarn
 yarn build
+npx lerna run --stream build
+
+# If in the js folder, cd up one level
+if [ -d "packages" ]
+then
+  cd ..
+fi
 
 HAS_DIST=$( git status | grep 'dist/' )
 
 if [ -n "$HAS_DIST" ]
 then
   echo "Some build artifacts found, making sure they are added to the commit. ${HAS_DIST}"
-  git add packages/*/dist/
+  git add js/packages/*/dist/
   # git commit --amend -C HEAD --no-verify
 fi
